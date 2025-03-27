e854d02f5c5954d23d3855f7d53a918553911545
unknown <Administrator@sjaw11v4901.sjcicd.com>
2025-03-27 10:19:38 -0700

    updated the simple test

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator function
 """
 
-def add(x,y):
+
+def add(x, y):
     """ Add x and  y return result """
     return x + y
+
 
 def substract(x, y):
     """ Subtract x and y return result """
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,16 +1,17 @@
-""" 
+"""
 sample tests
 """
 from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """ Test the calc module """
 
     def test_add_numbers(self):
         """ Test adding number together """
-        res = calc.add(8,6)
+        res = calc.add(8, 6)
 
         self.assertEqual(res, 14)
 
