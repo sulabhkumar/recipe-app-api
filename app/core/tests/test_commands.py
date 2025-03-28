f77a0fa33743d6411d48d8032aa2ab7350d42ab8
unknown <Administrator@sjaw11v4901.sjcicd.com>
2025-03-28 08:02:31 -0700

    write test for wait_for_db command

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,13 +1,13 @@
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2Error # type: ignore
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch('core.management.commands.wait_fordb.Command.check')
+@patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """"Test commands."""
 
