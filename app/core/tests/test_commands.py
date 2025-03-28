95645fd3f1f2e1434cdf3759fe7a1d4d5f24acc0
unknown <Administrator@sjaw11v4901.sjcicd.com>
2025-03-28 08:07:55 -0700

    write test for wait_for_db command

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,6 +1,6 @@
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error # type: ignore
+from psycopg2 import OperationalError as Psycopg2Error
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -9,7 +9,7 @@ from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    """"Test commands."""
+    """Test commands."""
 
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
@@ -21,8 +21,8 @@ class CommandTests(SimpleTestCase):
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        """Test waiting for databse when getting OpertionalError."""
-        patched_check.side_effect = ['Psycopg2OpError'] * 2 + \
+        """Test waiting for database when getting OperationalError."""
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
         
         call_command('wait_for_db')
