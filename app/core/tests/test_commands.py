357cb2047fa29be92fe106a9d95313a9b04a1867
unknown <Administrator@sjaw11v4901.sjcicd.com>
2025-03-28 08:53:03 -0700

    write test for wait_for_db command

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,6 +1,6 @@
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError # type: ignore
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
